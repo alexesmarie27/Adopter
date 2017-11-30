f2c08023902bf29be5f73b818eb95ffd025631e3
Alexes Presswood <ampress727@gmail.com>
2017-11-29 20:11:12 -0600

    Add Image variable to PetProfile Data Object

--- Adopter/app/src/main/java/apresswood/com/adopter/PetProfile.java
+++ Adopter/app/src/main/java/apresswood/com/adopter/PetProfile.java
@@ -1,25 +1,29 @@
 package apresswood.com.adopter;
 
 
+import android.media.Image;
+
 class PetProfile {
 
     //TODO use Jackson to automap values to variables once DB is set up
-    
+
     private String description;
     private String name;
     private int age;
     private String location;
     private String breed;
     private String gender;
+    private Image profilePicture;
 
     public PetProfile(String name, String description, int age, String location, String breed,
-                      String gender) {
+                      String gender, Image profilePicture) {
         this.description = description;
         this.name = name;
         this.age = age;
         this.location = location;
         this.breed = breed;
         this.gender = gender;
+        this.profilePicture = profilePicture;
     }
 
     String getDescription() {
@@ -44,5 +48,9 @@ class PetProfile {
 
     String getGender() {
         return gender;
+    }
+
+    Image getProfilePicture() {
+        return profilePicture;
     }
 }
