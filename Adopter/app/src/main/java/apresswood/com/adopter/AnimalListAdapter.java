a33c0bff476d10f8285d0b3c4c664f502c174402
Alexes Presswood <ampress727@gmail.com>
2017-11-29 20:22:32 -0600

    Set Card Data

--- Adopter/app/src/main/java/apresswood/com/adopter/AnimalListAdapter.java
+++ Adopter/app/src/main/java/apresswood/com/adopter/AnimalListAdapter.java
@@ -1,30 +1,33 @@
 package apresswood.com.adopter;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 import java.util.List;
 
 public class AnimalListAdapter extends BaseAdapter {
-
-
-    private List<PetProfile> petProfile;
+    private List<PetProfile> petProfiles;
     private MainActivity mainActivity;
 
-    private AnimalListAdapter(List<PetProfile> petProfile, MainActivity mainActivity) {
-        this.petProfile = petProfile;
+    private AnimalListAdapter(List<PetProfile> petProfiles, MainActivity mainActivity) {
+        this.petProfiles = petProfiles;
         this.mainActivity = mainActivity;
     }
 
     @Override
     public int getCount() {
-        return petProfile.size();
+        return petProfiles.size();
     }
 
     @Override
     public Object getItem(int position) {
-        return position;
+        return petProfiles.get(position);
     }
 
     @Override
@@ -34,10 +37,22 @@ public class AnimalListAdapter extends BaseAdapter {
 
     @Override
     public View getView(final int position, View convertView, ViewGroup parent) {
-
+        PetProfile petProfile = petProfiles.get(position);
         View rowView = convertView;
+        ImageView cardImage;
+        TextView cardDescription;
 
-        //TODO: lots of stuff
+        if (rowView == null) {
+            LayoutInflater inflater = mainActivity.getLayoutInflater();
+            rowView = inflater.inflate(R.layout.item, parent, false);
+
+            cardImage = rowView.findViewById(R.id.card_image);
+            cardDescription = rowView.findViewById(R.id.card_description);
+
+            cardDescription.setText(petProfile.getDescription());
+
+            Glide.with(mainActivity).load(petProfile.getProfilePicture()).into(cardImage);
+        }
 
         return rowView;
     }
