da8563bc16456e8df1f853ff470fc0956e794fe4
Alexes Presswood <ampress727@gmail.com>
2017-11-29 19:55:20 -0600

    Add Getters for PetProfile Data Object

--- Adopter/app/src/main/java/apresswood/com/adopter/PetProfile.java
+++ Adopter/app/src/main/java/apresswood/com/adopter/PetProfile.java
@@ -2,12 +2,24 @@ package apresswood.com.adopter;
 
 
 class PetProfile {
+
+    //TODO use Jackson to automap values to variables once DB is set up
+    
     private String description;
     private String name;
+    private int age;
+    private String location;
+    private String breed;
+    private String gender;
 
-    public PetProfile(String name, String description) {
+    public PetProfile(String name, String description, int age, String location, String breed,
+                      String gender) {
         this.description = description;
         this.name = name;
+        this.age = age;
+        this.location = location;
+        this.breed = breed;
+        this.gender = gender;
     }
 
     String getDescription() {
@@ -18,5 +30,19 @@ class PetProfile {
         return name;
     }
 
-    //TODO: lots of stuff
+    int getAge() {
+        return age;
+    }
+
+    String getLocation() {
+        return location;
+    }
+
+    String getBreed() {
+        return breed;
+    }
+
+    String getGender() {
+        return gender;
+    }
 }
