5736628c2fc40037465b01d91618da52dc592e2c
Alexes Presswood <ampress727@gmail.com>
2018-03-02 13:09:47 -0600

    Add Profile Options View

--- Adopter/app/src/main/java/apresswood/com/adopter/MainActivity.java
+++ Adopter/app/src/main/java/apresswood/com/adopter/MainActivity.java
@@ -1,6 +1,7 @@
 package apresswood.com.adopter;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -70,5 +71,10 @@ public class MainActivity extends AppCompatActivity {
                 adapter.notifyDataSetChanged();
             }
         });
+    }
+
+    protected void goToProfile(View view) {
+        Intent intent = new Intent(this, ProfileActivity.class);
+        startActivity(intent);
     }
 }
---
+++ Adopter/app/src/main/java/apresswood/com/adopter/ProfileActivity.kt
@@ -0,0 +1,13 @@
+package apresswood.com.adopter
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+
+class ProfileActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.human_profile)
+    }
+}
