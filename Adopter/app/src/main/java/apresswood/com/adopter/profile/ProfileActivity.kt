e269a9b8dd3112b9a7c242dd8251b883c8f3ae9c
Alexes Presswood <ampress727@gmail.com>
2018-03-02 15:21:01 -0600

    Add Header To Profile Screen

--- Adopter/app/src/main/java/apresswood/com/adopter/profile/ProfileActivity.kt
+++ Adopter/app/src/main/java/apresswood/com/adopter/profile/ProfileActivity.kt
@@ -4,14 +4,25 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import apresswood.com.adopter.FontManager
+import apresswood.com.adopter.MainActivity
 import apresswood.com.adopter.R
+import kotlinx.android.synthetic.main.edit_profile.*
 
 
 class ProfileActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.human_profile)
+        setContentView(R.layout.profile)
+
+        val iconFont = FontManager.getTypeface(applicationContext, FontManager.FONTAWESOME)
+        FontManager.markAsIconContainer(this.back_button, iconFont)
+    }
+
+    fun goBack(view: View) {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
     }
 
     fun goToEditProfile(view: View) {
