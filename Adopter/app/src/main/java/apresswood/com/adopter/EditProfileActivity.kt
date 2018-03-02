5b1aed5539ad2ff1258eed7efbbf8737fb27e82c
Alexes Presswood <ampress727@gmail.com>
2018-03-02 13:13:44 -0600

    Add Edit Profile Page

---
+++ Adopter/app/src/main/java/apresswood/com/adopter/EditProfileActivity.kt
@@ -0,0 +1,12 @@
+package apresswood.com.adopter
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class EditProfileActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.edit_profile)
+    }
+}
--- Adopter/app/src/main/java/apresswood/com/adopter/ProfileActivity.kt
+++ Adopter/app/src/main/java/apresswood/com/adopter/ProfileActivity.kt
@@ -1,7 +1,9 @@
 package apresswood.com.adopter
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 
 
 class ProfileActivity : AppCompatActivity() {
@@ -9,5 +11,10 @@ class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.human_profile)
+    }
+
+    fun goToEditProfile(view: View) {
+        val intent = Intent(this, EditProfileActivity::class.java)
+        startActivity(intent)
     }
 }
